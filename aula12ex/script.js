3b3543a912dc30be19aef3a4c5833610d3e32d61
Caroline Delay <146271511+CarolineDelay@users.noreply.github.com>
2024-01-24 20:48:08 -0300

    exercício aula 12

---
+++ aula12ex/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagens')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >=0 && hora < 12) {
+   // BOM DIA
+    img.src = 'fotomanha.jpg'
+  } else if (hora >= 12 && hora < 18) {
+    // BOA TARDE
+    img.src= 'fototarde.jpg'
+  } else {
+    // BOA NOITE
+    img.src = 'fotonoite.jpg'
+  }
+}
